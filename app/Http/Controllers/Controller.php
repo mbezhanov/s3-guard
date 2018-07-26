2d9c3e6bdd8b384a701908b41c3328609c434883
Marin Bezhanov <marin.bezhanov@gmail.com>
2018-07-26 17:10:17 +0300

    cache images, scripts and stylesheets

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Host;
+use Carbon\Carbon;
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Filesystem\FilesystemManager;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\Request;
@@ -39,11 +41,13 @@ class Controller extends BaseController
         if (!$bucket->exists($path)) {
             throw new NotFoundHttpException();
         }
+        $metadata = $bucket->getMetadata($path);
+        $headers = $this->buildHeadersFromMetadata($metadata);
 
-        return response($bucket->get($path), Response::HTTP_OK, ['Content-Type' => $bucket->getMimetype($path)]);
+        return response($bucket->get($path), Response::HTTP_OK, $headers);
     }
 
-    private function retrieveBucketForHost(string $hostname)
+    private function retrieveBucketForHost(string $hostname): Filesystem
     {
         $bucket = Host::where('name', $hostname)->first();
 
@@ -57,5 +61,24 @@ class Controller extends BaseController
             'region' => $bucket->region_name,
             'bucket' => $bucket->bucket_name,
         ]);
+    }
+
+    private function buildHeadersFromMetadata(array $metadata): array
+    {
+        $mimetype = $metadata['mimetype'] ?? 'text/plain';
+
+        $headers = [];
+        $headers['Content-Type'] = $mimetype;
+        $headers['Cache-Control'] = $this->buildCacheControlHeader($mimetype);
+        $headers['Last-Modified'] = Carbon::createFromTimestamp($metadata['timestamp'] ?? time())->toRfc7231String();
+
+        return $headers;
+    }
+
+    private function buildCacheControlHeader(string $mimetype): string
+    {
+        $cacheableMimeTypes = ['application/javascript', 'text/css', 'image/jpeg', 'image/png', 'image/gif', 'image/svg+xml', 'image/x-icon'];
+
+        return in_array($mimetype, $cacheableMimeTypes) ? 'private, max-age=604800' : 'private, no-cache';
     }
 }
